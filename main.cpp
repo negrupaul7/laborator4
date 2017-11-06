2659702d3482255de9f8f891d6c8bbd3c4b38e5e
paul <negrupaul7@gmail.com>
2017-11-07 01:07:30 +0200

    exercitiul4

--- main.cpp
+++ main.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 int main()
 {
-    int a[20], nr, i, j, aux;
+    int a[20], nr, i, aux;
     cout << "Numarul de elemente din sirul a (maxim 20) : ";
     cin >> nr;
     for (i = 0; i < nr; i++) {
         cout << "a[" << i << "] = ";
         cin >> a[i];
     }
-    for (i = 0, j = nr - 1; i < j; i++, j--)
+    aux = a[0];
+    for (i = 0; i < nr - 1; i++)
     {
-        aux = a[i];
-        a[i] = a[j];
-        a[j] = aux;
+        a[i] = a[i+1];
     }
+    a[nr-1] = aux;
     for (i = 0; i < nr; i++)
     {
         cout << a[i] << " ";
